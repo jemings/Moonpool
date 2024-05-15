b466d568cb2d9e291f199e7da534646fa6f228d1
Jemings Ko <jemings@naver.com>
2024-05-15 19:21:27 +0900

    Add SetImage
    Signed-off-by: Jemings Ko <jemings@naver.com>

--- Moonpool.Test/ModelsTest.cs
+++ Moonpool.Test/ModelsTest.cs
@@ -8,17 +8,17 @@ namespace Moonpool.Test
         public void Question_all_correct()
         {
             Question q = new Question { answer = "3" };
-            q.receiveSolution("3");
-            Assert.Equal(100, q.getCorrectRate());
+            q.ReceiveSolution("3");
+            Assert.Equal(100, q.GetCorrectRate());
         }
 
         [Fact]
         public void Question_half_correct()
         {
             Question q = new Question { answer = "3" };
-            q.receiveSolution("2");
-            q.receiveSolution("3");
-            var rate = q.getCorrectRate();
+            q.ReceiveSolution("2");
+            q.ReceiveSolution("3");
+            var rate = q.GetCorrectRate();
             Assert.Equal(50, rate);
         }
 
@@ -26,30 +26,30 @@ namespace Moonpool.Test
         public void Question_one_third_correct()
         {
             Question q = new Question { answer = "3" };
-            q.receiveSolution("1");
-            q.receiveSolution("2");
-            q.receiveSolution("3");
-            Assert.Equal(((decimal)1 / 3) * 100, q.getCorrectRate());
+            q.ReceiveSolution("1");
+            q.ReceiveSolution("2");
+            q.ReceiveSolution("3");
+            Assert.Equal(((decimal)1 / 3) * 100, q.GetCorrectRate());
         }
 
         [Fact]
         public void Weight_1_with_all_correct()
         {
             Question q = new Question { answer = "3", weight = 1 };
-            q.receiveSolution("3");
-            q.receiveSolution("3");
-            q.receiveSolution("3");
-            Assert.Equal(100, q.getWeightedRate());
+            q.ReceiveSolution("3");
+            q.ReceiveSolution("3");
+            q.ReceiveSolution("3");
+            Assert.Equal(100, q.GetWeightedRate());
         }
 
         [Fact]
         public void Weight_dot_3_with_all_correct()
         {
             Question q = new() { answer = "3", weight = (decimal)0.3 };
-            q.receiveSolution("3");
-            q.receiveSolution("3");
-            q.receiveSolution("3");
-            Assert.Equal(30, q.getWeightedRate());
+            q.ReceiveSolution("3");
+            q.ReceiveSolution("3");
+            q.ReceiveSolution("3");
+            Assert.Equal(30, q.GetWeightedRate());
         }
     }
 }
--- Moonpool/Models/Question.cs
+++ Moonpool/Models/Question.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Security.Cryptography;
 
 namespace Moonpool.Models
 {
@@ -13,19 +16,64 @@ namespace Moonpool.Models
         public string? answer;
         private uint totalSolvedNumber;
         private uint numberOfCorrect;
+        public string? imageHash;
         public decimal weight;
 
-        public decimal getCorrectRate()
+        private static byte[] GetImageByte(string imagePath)
+        {
+            byte[] imageBytes;
+            using (FileStream fileStream = new(imagePath, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bufferedStream = new(fileStream))
+            {
+                using (MemoryStream memoryStream = new())
+                {
+                    bufferedStream.CopyTo(memoryStream);
+                    imageBytes = memoryStream.ToArray();
+                }
+            }
+            return imageBytes;
+        }
+
+        private static string GetImageHash(byte[] imageBytes)
+        {
+            string hashString;
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(imageBytes);
+                hashString = BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+            return hashString;
+        }
+
+        private static Image GetImage(byte[] imageBytes)
+        {
+            BitmapImage bitmapImage = new();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.StreamSource = new MemoryStream(imageBytes);
+            bitmapImage.EndInit();
+
+            return new Image { Source = bitmapImage };
+        }
+
+        public void SetImage(string imagePath)
+        {
+            var imageBytes = GetImageByte(imagePath);
+            imageHash = GetImageHash(imageBytes);
+            image = GetImage(imageBytes);
+        }
+
+        public decimal GetCorrectRate()
         {
             return ((decimal)numberOfCorrect/totalSolvedNumber) * 100;
         }
 
-        public decimal getWeightedRate()
+        public decimal GetWeightedRate()
         {
-            return getCorrectRate() * weight;
+            return GetCorrectRate() * weight;
         }
 
-        public void receiveSolution(string solution)
+        public void ReceiveSolution(string solution)
         {
             totalSolvedNumber++;
             if (solution == answer)
